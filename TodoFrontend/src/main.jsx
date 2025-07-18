76807cbdb3414232b80d221a3d204328c0f0c957
tkocoglu <tayyipkocoglu4@gmail.com>
2025-07-18 07:55:57 +0300

    Update main.jsx

--- TodoFrontend/src/main.jsx
+++ TodoFrontend/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // App bileşenini doğru yerden çekiyor mu?
-import './index.css' // Global stil dosyasını çekiyor mu?
+import App from './App.jsx' 
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App /> {/* App bileşeni render ediliyor mu? */}
+    <App /> 
   </React.StrictMode>,
 )
